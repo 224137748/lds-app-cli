649e140b71e05d46b8937aee96d88fa9a69d9835
ZhangJunrui <zhangjunrui@ludashi.com>
2021-06-01 09:53:33 +0800

    网上donwoload文件

--- bin/index.js
+++ bin/index.js
@@ -7,20 +7,28 @@ const clear = require('clear');
 const program = require('commander');
 const updateCheckPackage = require('../lib/update');
 const setMirror = require('../lib/mirror');
+const downloadTemplate = require('../lib/download');
 
 program.version(require('../package.json').version, '-v, --version');
 
-
+// 检查版本更新
 program
   .command('upgrade')
   .description('Check the lds-app-cli version.')
   .action(() => updateCheckPackage());
 
 
+// 设置镜像
 program
   .command('mirror <template_mirror>')
   .description('Set the template mirror.')
   .action((tplMirror) => setMirror(tplMirror));
+
+// 下载模板
+program
+  .command('template')
+  .description('Download template from mirror.')
+  .action(() => downloadTemplate());
 
 // 解析命令行参数
 program.parse(process.argv);
