f22c2e2d4655abfc8682f82ef7abedad6a3064f6
ZhangJunrui <zhangjunrui@ludashi.com>
2021-06-03 18:49:07 +0800

    增加create命令，未完成，模板copy

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
 const { promisify } = require('util');
-const figlet = promisify(require('figlet'));
-// 清空控制台命令行
-const clear = require('clear');
+
 // 加载commander 库
 const program = require('commander');
 const updateCheckPackage = require('../lib/update');
-const setMirror = require('../lib/mirror');
-const downloadTemplate = require('../lib/download');
 const initProject = require('../lib/init');
 const start = require('../lib/start');
+const create = require('../lib/create');
 
 program.version(require('../package.json').version, '-v, --version');
 
@@ -20,33 +17,27 @@ program
   .action(() => updateCheckPackage());
 
 
-// 设置镜像
-program
-  .command('mirror <template_mirror>')
-  .description('Set the template mirror.')
-  .action((tplMirror) => setMirror(tplMirror));
-
-// 下载模板
-program
-  .command('template')
-  .description('Download template from mirror.')
-  .action(() => downloadTemplate());
-
-
 
 // init 初始化项目
 program
   .name('lds-app-cli')
   .usage('<commands> [options]')
-  .command('init <project_path>')
-  .description('Create a web App.')
+  .command('init <project_name>')
+  .description('Initialize a project.')
   .action(project => initProject(project));
+
+
+// 创建项目
+program
+  .command('create <app_name>')
+  .description('Create a web app.')
+  .action(projectName => create(projectName));
 
 // 启动项目
 program
   .command('start <project_path>')
   .option('-p --port <port>', '端口', 3000)
-  .description('Running project.')
+  .description('Running a web app.')
   .action((projectPath, { port }) => start(projectPath, port));
 
 // 解析命令行参数
